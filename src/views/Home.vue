17c28763909b9dbfde84d275c967bc7c7d533cd9
Coder-XO <coderless@gmail.com>
2021-10-23 15:29:38 +0800

    删除了无用文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    我是首页!
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+// vant 组件的使用
+import Vue from 'vue'
+import { Button } from 'vant';
+Vue.use(Button)
+import 'vant/lib/button/style';
 export default {
   name: 'Home',
   components: {
